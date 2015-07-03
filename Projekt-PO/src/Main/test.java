1b61618fbfcdd52dec95c7065b45b4c0453498ad
Maciej Kocyla <maciej.kocyla@gmail.com>
2015-07-03 00:00:12 -0700

    first view

--- Projekt-PO/src/Main/test.java
+++ Projekt-PO/src/Main/test.java
@@ -6,60 +6,128 @@ import java.util.Scanner;
 import pacjent1.*;
 import lekarz.*;
 
-public class test implements Serializable{
+
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class test extends JFrame implements Serializable {
+	
+    public test() {
+        initUI();
+    }
+
+    private void initUI() {
+        
+    	JButton quitButton = new JButton("Quit");
+    	quitButton.setToolTipText("Leave the application");
+    	
+    	quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+    	
+    	createLayout(quitButton);
+    	
+        setTitle("Przychodnia");
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    
+    private void createLayout(JComponent... arg) {
+
+        Container pane = getContentPane();
+        GroupLayout gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+    }
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException{
-		while(true){
-			System.out.println("MENU");
-			System.out.println("1. Dodanie nowego pacjenta");
 			
-			Scanner numer = new Scanner(System.in);
-			int Opcja = numer.nextInt();
+		EventQueue.invokeLater(new Runnable() {
+	        
+            @Override
+            public void run() {
+                test layout = new test();
+                layout.setVisible(true);
+            }
+        });
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		/*
+		System.out.println("MENU");
+		System.out.println("1. Dodanie nowego pacjenta");
+		
+		Scanner numer = new Scanner(System.in);
+		int Opcja = numer.nextInt();
+    
+		switch(Opcja){
+		case 1:
+			Pacjent a = new Pacjent();
+			Pacjent nowy = new Pacjent();
+			nowy = a.Nowy();
+			
+			System.out.println(" ");
+			System.out.println(" ");
+			System.out.println(" ");
+			System.out.println(" ");
+			System.out.println(" ");
+			System.out.println(" ");
+			System.out.println("Poda³eœ nastêpuj¹ce dane: ");
+			
+			nowy.Wyœwietl();
+			
+			System.out.println(" ");
+			System.out.println(" ");
+			System.out.println(" ");
+			System.out.println("Czy dane siê zgadzaj¹?");
+	
+			Scanner potw = new Scanner(System.in);
+			String Odp = potw.nextLine();
 	    
-			switch(Opcja){
-			case 1:
-				Pacjent a = new Pacjent();
-				Pacjent nowy = new Pacjent();
-				nowy = a.Nowy();
-				
+			if(Odp.equals("tak") || Odp.equals("TAK") || Odp.equals("Tak")) {
+				FileWriter zapis = new FileWriter("Pacjenci.txt", true);
+			    BufferedWriter out = new BufferedWriter(zapis);
+			    out.write(nowy.Imiê() + "\t" + nowy.Nazwisko() + "\t" + nowy.PESEL() + "\t" + nowy.Adres.Ulica() + "\t" + nowy.Adres.NrUlicy() + "\\" + nowy.Adres.NrDomu() + "\t" + nowy.Adres.Miejscowoœæ());
+			    out.close();
+			    
+				System.out.println("Dodano!");
 				System.out.println(" ");
 				System.out.println(" ");
 				System.out.println(" ");
-				System.out.println(" ");
-				System.out.println(" ");
-				System.out.println(" ");
-				System.out.println("Poda³eœ nastêpuj¹ce dane: ");
-				
-				nowy.Wyœwietl();
-				
-				System.out.println(" ");
-				System.out.println(" ");
-				System.out.println(" ");
-				System.out.println("Czy dane siê zgadzaj¹?");
-    	
-				Scanner potw = new Scanner(System.in);
-				String Odp = potw.nextLine();
-		    
-				if(Odp.equals("tak") || Odp.equals("TAK") || Odp.equals("Tak")) {
-					FileWriter zapis = new FileWriter("Pacjenci.txt", true);
-				    BufferedWriter out = new BufferedWriter(zapis);
-				    out.write(nowy.Imiê() + "\t" + nowy.Nazwisko() + "\t" + nowy.PESEL() + "\t" + nowy.Adres.Ulica() + "\t" + nowy.Adres.NrUlicy() + "\\" + nowy.Adres.NrDomu() + "\t" + nowy.Adres.Miejscowoœæ());
-				    out.close();
-				    
-					System.out.println("Dodano!");
-					System.out.println(" ");
-					System.out.println(" ");
-					System.out.println(" ");
-				}
-		    
-				else {
-					System.out.println("Dane nie zosta³y zapisane. WprowadŸ ponownie dane");
-					Opcja = 1;
-				}
-	    	
-				break;
-	    	
-			}	        
-		}
+			}
+	    
+			else {
+				System.out.println("Dane nie zosta³y zapisane. WprowadŸ ponownie dane");
+				Opcja = 1;
+			}
+			*/
+	    	        
 	}
 }
